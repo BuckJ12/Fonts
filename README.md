77ca4a4c8c01b545da745640fffb92390265fab3
BuckJ12 <cyanblanket@gmail.com>
2023-10-17 12:01:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Fonts
+ Small Program to View the Available Fonts in Java
---
+++ src/FontViewer.java
@@ -0,0 +1,62 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class FontViewer {
+
+    private static int fontIndex = 0;
+    private static String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+    private static JLabel label = new JLabel();
+    private static JPanel panel = new JPanel();
+    private static JTextArea outputTextArea;
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Font Viewer");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setLayout(new BorderLayout());
+
+            JButton nextButton = new JButton("Next Font");
+            nextButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    fontIndex = (fontIndex + 1) % fonts.length;
+                    updateFont();
+                }
+            });
+
+            JButton prevButton = new JButton("Previous Font");
+            prevButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    fontIndex = (fontIndex - 1 + fonts.length) % fonts.length;
+                    updateFont();
+                }
+            });
+            outputTextArea = new JTextArea();
+            outputTextArea.setEditable(false);
+            outputTextArea.setText("ABCDEFGHIJKLMNOPQRSTUVWXYZ\nabcdefghijklmnopqrstuvwxyz");
+
+            frame.add(prevButton, BorderLayout.WEST);
+            panel.add(label);
+            frame.add(panel, BorderLayout.SOUTH);
+            frame.add(nextButton, BorderLayout.EAST);
+            frame.add(outputTextArea, BorderLayout.CENTER);
+
+            updateFont();
+
+            frame.setPreferredSize(new Dimension(1200, 900));
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
+    }
+
+    private static void updateFont() {
+        String fontName = fonts[fontIndex];
+        label.setText(fontName);
+        label.setFont(new Font(fontName, Font.PLAIN, 20));
+        outputTextArea.setFont(new Font(fontName, Font.PLAIN, 32));
+    }
+}
